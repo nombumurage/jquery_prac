917ae2e4933c7d23ce616c1b9ae16fd89e80ab4f
nombumurage <nombumurage@gmail.com>
2016-11-23 10:41:24 +0300

    Connceting js-jquery to html

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+jQuery(document).ready(function () {
+    jQuery("h1").click(function () {
+        alert("This is a header");
+        alert("I told you this is a HEADER!!!!!!!!!DUMBASS");
+    });
+    jQuery("p").click(function () {
+        alert("This is a paragraph");
+    });
+    jQuery("img").click(function () {
+        alert("This is an image");
+    });
+});
