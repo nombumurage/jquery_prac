2ce92e124bd30e63e16fad4bf0d24cc751fd230a
nombumurage <nombumurage@gmail.com>
2016-11-24 09:07:56 +0300

    Cipher upto the point of function four. To be complteted

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,25 @@
 jQuery(document).ready(function () {
-    jQuery("h1").click(function () {
-        alert("This is a header");
-        alert("I told you this is a HEADER!!!!!!!!!DUMBASS");
-    });
-    jQuery("p").click(function () {
-        alert("This is a paragraph");
-    });
-    jQuery("img").click(function () {
-        alert("This is an image");
-    });
-});
+    var sentence = prompt("Enter a sentence please");
+    alert(sentence);
 
-var sentence = prompt("Enter a sentence please");
-var output = document.write(sentence);
-var first = output.charAt(0);
+    var first = sentence.charAt(0).toUpperCase();
+    var last = sentence.slice(-1).toUpperCase();
+    var sent1 = first + last;
+    alert(sent1);
+
+    function reverse(sent1) {
+        return sent1.split("").reverse().join("");
+    };
+    alert(reverse(sent1));
+
+    var sent4 = reverse(sent1);
+    var revadd = sentence + sent4
+    alert(revadd);
+
+
+    var four = function (sentence) {
+        var length = sentence.length;
+        var position = charAt(length / 2);
+        alert(position + revadd);
+    }
+});
